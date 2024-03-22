b8a616a5cc66a5edcaf9d107fdb5997d2d7765b4
Kevin <kevindubon43@gmail.com>
2024-03-22 15:00:22 -0700

    Add index to img

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -17,10 +17,21 @@ function Cards() {
                 return response.json();
             }));
             setPokemon(pokemonDetails);
-            console.log(pokemon)
         } catch (error) {
             console.log(error);
         }
+    }
+
+    function shuffleCards() {
+        //code will scramble the card divs in a random order.
+    }
+
+    const handleClick = (index, name) => {
+        //if card is clicked for the first time, add a point to score and mark card as clicked
+        //if same card is clicked again, score = 0 and mark all cards as unclicked.
+        console.clear('');
+        console.log(name, index)
+        shuffleCards();
     }
 
     return (
@@ -28,12 +39,12 @@ function Cards() {
             <div className="cardsContainer">
                 {pokemon.map((poke, index) => (
                     <div className="pokemonCard" key={index}>
-                        <img src={poke.sprites.other.dream_world.front_default} alt={poke.name} />
+                        <img onClick={() => handleClick(index, poke.name)} index={index} src={poke.sprites.other.dream_world.front_default} alt={poke.name} />
                     </div>
                 ))}
             </div>
         </>
     )
 }
-
+//
 export { Cards };
